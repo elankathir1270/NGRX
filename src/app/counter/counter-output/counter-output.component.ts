d0e4be9e6c9fa7308b0d54a310045578a41202a4
elankathir <esekar@diligentsquad.com>
2024-05-30 19:29:58 +0530

    refactor in counter

--- src/app/counter/counter-output/counter-output.component.ts
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { CounterState } from '../state/counter.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter-output',
@@ -9,13 +11,17 @@ import { Store } from '@ngrx/store';
 export class CounterOutputComponent {
 
   counter: number;
+  counter$ : Observable<{counter : number}>;
 
-  constructor(private store : Store<{counter : {counter : number}}>) {}
+  constructor(private store : Store<{counter : CounterState}>) {}
 
   ngOnInit() {
+
     this.store.select('counter').subscribe((data) => {
       this.counter = data.counter;
     })
+
+    this.counter$ = this.store.select('counter');
   }
 
 }
