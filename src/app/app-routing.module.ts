ada8ec3977257ecc582eafb882bf76d1a228a7af
elankathir <esekar@diligentsquad.com>
2024-06-04 16:23:10 +0530

    implementation of lazy loading for the routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter/counter.component';
-import { PostlistComponent } from './post/postlist/postlist.component';
-import { AddPostComponent } from './post/add-post/add-post.component';
-import { UpdatePostComponent } from './post/update-post/update-post.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "counter", component: CounterComponent},
-  {path: "postlist", component : PostlistComponent,
-   children: [
-    {path: "add", component: AddPostComponent},
-    {path: "update/:id", component: UpdatePostComponent}
-
-   ]
+  {path: "counter",
+    loadChildren : () => import('./counter/counter.module').then(m => m.CounterModule)
+  },
+  {path: "postlist",
+  loadChildren: () => import('./post/post.module').then(m => m.PostModule),
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter/counter.component';
-import { CounterButtonsComponent } from './counter/counter-buttons/counter-buttons.component';
-import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
 import { StoreModule } from '@ngrx/store';
-import { counterReducer } from './counter/state/counter.reducer';
-import { CustomCounterInputComponent } from './counter/custom-counter-input/custom-counter-input.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import { PostlistComponent } from './post/postlist/postlist.component';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { appReducer } from './store/app.state';
-import { AddPostComponent } from './post/add-post/add-post.component';
-import { UpdatePostComponent } from './post/update-post/update-post.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent,
-    CounterButtonsComponent,
-    CounterOutputComponent,
-    CustomCounterInputComponent,
     HomeComponent,
     HeaderComponent,
-    PostlistComponent,
-    AddPostComponent,
-    UpdatePostComponent
   ],
   imports: [
     BrowserModule,
