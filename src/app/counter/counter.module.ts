6a302556774a614d33fef0dcad6befb027da839d
elankathir <esekar@diligentsquad.com>
2024-06-04 16:55:33 +0530

    lazy loading state using forFeature store module

--- src/app/counter/counter.module.ts
+++ src/app/counter/counter.module.ts
@@ -6,6 +6,8 @@ import { CounterButtonsComponent } from './counter-buttons/counter-buttons.compo
 import { CounterOutputComponent } from './counter-output/counter-output.component';
 import { CustomCounterInputComponent } from './custom-counter-input/custom-counter-input.component';
 import { FormsModule } from '@angular/forms';
+import { StoreModule } from '@ngrx/store';
+import { counterReducer } from './state/counter.reducer';
 
 
 const routes : Routes = [
@@ -23,7 +25,8 @@ const routes : Routes = [
   imports: [
     CommonModule,
     FormsModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    StoreModule.forFeature('counter',counterReducer)
   ]
 })
 export class CounterModule { }
